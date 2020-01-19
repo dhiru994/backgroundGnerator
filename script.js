0e56d50cda1918307d139931b4dba0749c66069a
dhiraj <vemuridhirajsai@gmail.com>
2020-01-19 07:28:54 -0500

    created methods in javascript adding books and displaying books

--- script.js
+++ script.js
@@ -12,7 +12,25 @@ class Book{
 // ui class:handle ui tasks
 
 class UI{
+//all the mesthod are staic because we are instantiating the classes
 	static displayBooks(){
+
+		//creating an array of objects for example books with objects
+		//properties with title,author,book
+		const storedBooks =[
+			{
+				title:'Book one',
+				author:'Jhon Doe',
+				isbn:'1234'
+			},
+			{
+				title:'Book two',
+				author:'dhiru',
+				isbn:'124'
+			}
+		];
+		const books=storedBooks;
+		books.forEach((book)=>UI.addBookToList(book));
 
 	}
 	static addBookToList(book){
